c48a0c8ff1b43cd0fd51791fd90cbb9abe037f39
Rafael Luiz <rafaelluiz.info@gmail.com>
2024-11-25 13:36:32 +0000

    feat: adiciona os scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto feito do 0 para aprimoramento. Inspirado em https://tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Deus seja louvado! Amém!</h1>
+  return <h1>Deus seja louvado! Amém!</h1>;
 }
 
 export default Home;
